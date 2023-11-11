c70b659baefaaee26305681fbfbb71133e19c602
DunaAlsuliman <79339398+DunaAlsuliman@users.noreply.github.com>
2023-11-12 00:22:47 +0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/new_sample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/new_sample/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.new_sample;
+package com.calcios.new_sample;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.new_sample", appContext.getPackageName());
+        assertEquals("com.calcios.new_sample", appContext.getPackageName());
     }
 }
